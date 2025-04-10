673d7360ac50aa13234e3aa57c7679517fbc1fcf
emils <esla22@student.aau.dk>
2025-04-10 11:45:27 +0200

    added support for a few different types of prompts

--- app/gpt.py
+++ app/gpt.py
@@ -1,31 +1,32 @@
 from openai import OpenAI
 import json
 
-test_name_LASKI = "{\"given-name\": \"Zigmund\", \"surname\": \"Ehrlich\"}"
-test_name_Zylbercweig = "{\"given-name\": \"Zygmund\", \"surname\": \"Ehriykh\"}"
+test_name_LASKI = '{"given-name": "Zigmund", "surname": "Ehrlich"}'
+test_name_Zylbercweig = '{"given-name": "Zygmund", "surname": "Ehriykh"}'
+# test_name_LASKI = '{"given-name": "Chaim", "surname": "Abramovitsh"}'
+# test_name_Zylbercweig = '{"given-name": "shium-yekb", "surname": "abramovytsh"}'
 
-class Developer_strings():
-    developer_string1 = "You are a language model that does entity resolution, You will be given two names split up into name parts, one from the Zylbercweig dataset and one from the LASKI dataset. The Zylbercweig dataset contains names written in the hebrew alphabet which have been transliterated into the roman alphabet, while the LASKI dataset contains names written in the roman alphabet. Your task is to determine whether the two names refer to the same person. Reply only with \"True\" or \"False\""
+
+class Developer_strings:
+    developer_string1 = 'You are a language model that does entity resolution, You will be given two names split up into name parts, one from the Zylbercweig dataset and one from the LASKI dataset. The Zylbercweig dataset contains names written in the hebrew alphabet which have been transliterated into the roman alphabet, while the LASKI dataset contains names written in the roman alphabet. Your task is to determine whether the two names refer to the same person. Reply only with "True" or "False"'
+    developer_string2 = 'You will be given two Python dictionaries containing name parts. Reply with "True" if the name parts in both dictionaries refer the same person and "False" otherwise. The name parts may refer to the same person even if their spelling is not exactly the same. Explain your answer in 30 words or less.'
+    developer_string3 = 'You will be given two names. The first name has been transliterated from the hebrew alphabet and may be different from the second name even if the names refer to the same person. Your task is to determine if the names refer to the same person and respond with "True" if they do and "False" otherwise. Explain your answer in 30 words or less.'
 
 
 def prompt(client, developer_prompt, user_prompt, tokens=200):
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "developer",
-            "content": [{
-                "type": "text",
-                "text": developer_prompt
-            }]},
-            {"role": "user",
-            "content": [{
-                "type": "text",
-                "text": user_prompt
-            }]},
+            {
+                "role": "developer",
+                "content": [{"type": "text", "text": developer_prompt}],
+            },
+            {"role": "user", "content": [{"type": "text", "text": user_prompt}]},
         ],
-        max_tokens = tokens
+        max_tokens=tokens,
     )
     return completion.choices[0].message.content
+
 
 def string_from_name_parts(name_parts):
     return_string = "{"
@@ -34,21 +35,58 @@ def string_from_name_parts(name_parts):
         return_string += ": "
         return_string += name_parts[entry]
         return_string += ", "
-    return_string = return_string[0:len(return_string)-2]
+    return_string = return_string[0 : len(return_string) - 2]
     return_string += "}"
     return return_string
 
-def create_prompt_string(Zylbercweig_name_parts, LASKI_name_parts):
-    return "Do these two names refer to the same person?\n" + "Name from the Zylbercweig dataset: " + string_from_name_parts(Zylbercweig_name_parts) + "\nName from the LASKI dataset: " + string_from_name_parts(LASKI_name_parts)
+
+def name_from_name_parts(name_parts):
+    return_string = ""
+    for entry in name_parts:
+        return_string += name_parts[entry]
+        return_string += " "
+    return_string = return_string[:-1]
+    return return_string
+
+
+def create_prompt_string(Zylbercweig_name_parts, LASKI_name_parts, pattern=0):
+    match pattern:
+        case 0:
+            return (
+                "Do these two names refer to the same person?\n"
+                + "Name from the Zylbercweig dataset: "
+                + string_from_name_parts(Zylbercweig_name_parts)
+                + "\nName from the LASKI dataset: "
+                + string_from_name_parts(LASKI_name_parts)
+            )
+        case 1:
+            return (
+                string_from_name_parts(Zylbercweig_name_parts)
+                + " "
+                + string_from_name_parts(LASKI_name_parts)
+            )
+        case 2:
+            return (
+                "First name: "
+                + name_from_name_parts(Zylbercweig_name_parts)
+                + " Second name: "
+                + name_from_name_parts(LASKI_name_parts)
+            )
+        case _:
+            assert False  # we should never reach this case
+
 
 if __name__ == "__main__":
-    with open('secrets.json', 'r') as file:
+    developer_string = Developer_strings.developer_string3
+    prompt_string = create_prompt_string(
+        json.loads(test_name_Zylbercweig), json.loads(test_name_LASKI), pattern=2
+    )
+    with open("secrets.json", "r") as file:
         secrets = json.load(file)
     client = OpenAI(
-        organization = secrets["organization"],
-        project = secrets["project"],
-        api_key = secrets["api_key"],
-        )
-    prompt_string = create_prompt_string(json.loads(test_name_Zylbercweig), json.loads(test_name_LASKI))
-    answer = prompt(client, Developer_strings.developer_string1, prompt_string, 20)
-    print(answer)
+        organization=secrets["organization"],
+        project=secrets["project"],
+        api_key=secrets["api_key"],
+    )
+    answer = prompt(client, developer_string, prompt_string, 200)
+    print(f"Developer:\n{developer_string}\nUser:\n{prompt_string}\nAnswer:\n{answer}")
